e02245fa0e43b9ca670f69ee08646115933030c3
StephenStolk <shikharburman2@gmail.com>
2024-09-09 22:36:29 +0530

    Updated Landing page

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import Home from './Home'
+import Services from './Services'
 
 const Landing = () => {
   return (
     <>
     <Navbar />
+    <Home />
+    <Services />
     </>
   )
 }
