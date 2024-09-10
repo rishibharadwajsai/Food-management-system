69bf881363edcf51b082cd09e51b294a438285af
Rishi Bharadwaj Sai Lella <rishi.lella123@gmail.com>
2024-09-10 11:39:50 +0530

    Revert "Updates"
    This reverts commit 8f63625817eacedda007ab2f488fc03d03321c2e.

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: 'http://192.168.63.4:5173',  // Replace with the URL of your frontend
+    origin: 'http://localhost:5173',  // Replace with the URL of your frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
 }));
