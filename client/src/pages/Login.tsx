cd91e2e6f36fd6adb6983badec657c4a2924ac87
Rishi Bharadwaj Sai Lella <rishi.lella123@gmail.com>
2024-09-10 20:07:00 +0530

    Updated Home Page UI

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "../App.css";
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -40,7 +41,7 @@ const Login = () => {
   return (
     <form
       onSubmit={onSubmit}
-      className="flex flex-col justify-center align-middle items-center"
+      className="flex flex-col justify-center align-middle items-center space-y-5"
     >
       <input
         type="email"
@@ -49,7 +50,7 @@ const Login = () => {
         onChange={onChange}
         placeholder="Email"
         required
-        className=" border-2 border-black rounded-md text-black font-normal"
+        className="rounded-md text-black home-content-bg p-1"
       />
       <input
         type="password"
@@ -58,15 +59,35 @@ const Login = () => {
         onChange={onChange}
         placeholder="Password"
         required
-        className=" border-2 border-black rounded-md text-black font-normal"
+        className="rounded-md text-black home-content-bg p-1"
       />
       <button
         type="submit"
-        className="bg-blue-500 text-white rounded-lg w-20 font-semibold"
+        className="
+        hover:bg-blue-500 text-black border-2 border-gray-600 hover:text-white rounded-lg w-20 font-semibold p-2"
       >
         Login
       </button>
-      {message && <p>{message}</p>}
+      {message && (
+        <div
+          className="flex items-center p-4 my-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+          role="alert"
+        >
+          <svg
+            className="flex-shrink-0 inline w-4 h-4 me-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          </svg>
+          <span className="sr-only">Info</span>
+          <div>
+            <span className="font-medium">{message}</span>
+          </div>
+        </div>
+      )}
     </form>
   );
 };
