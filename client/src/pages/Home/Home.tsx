34bbd5b802ac456c45f684fd00a8e8dacb103e8e
StephenStolk <shikharburman2@gmail.com>
2024-09-12 11:24:27 +0530

    Updated Home component

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Navbar from './components/Navbar'
+
+const Home = () => {
+  return (
+   <>
+    <Navbar />
+    <Hero />
+    <Services />
+   </>
+  )
+}
+
+export default Home
